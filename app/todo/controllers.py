b8380c0d5a5dcffb7f038dfa4dfd6a8226d885a1
leensungzero <brianwoo119@naver.com>
2019-02-13 20:45:30 +0900

    ADD: Todo create 후 DB에 추가

--- app/todo/controllers.py
+++ app/todo/controllers.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, flash
 
 from app.todo.forms import TodoForm
+from app.todo.model.models import Todo
 
 todo = Blueprint('todo', __name__)
 
@@ -9,7 +10,14 @@ def create():
     form = TodoForm(request.form)
 
     if form.validate_on_submit():
+        # Todo: json validator 추가
+        # Todo: github 참고 후 exception
+
+        title = request.form['title']
+        name = request.form['content']
+        Todo.add_todo(title, name)
         flash('글 작성 완료')
+        return 'success'
 
     flash('글 작성 중 오류 발생', 'error-message')
 
