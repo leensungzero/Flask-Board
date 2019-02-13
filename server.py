54c2775560544edd2fb89aca97e20f0b24c2d301
leensungzero <brianwoo119@naver.com>
2019-02-13 17:47:44 +0900

    MOD: server run config modified

--- server.py
+++ server.py
@@ -1,12 +1,3 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+app.run(port=5000, debug=True)
