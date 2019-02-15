2c4582f925b79bd106051a956a71f56d9d8361d9
leensungzero <brianwoo119@naver.com>
2019-02-15 19:42:40 +0900

    ADD: login_manager 등록

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 from app.todo.controllers import todo as todo_module
